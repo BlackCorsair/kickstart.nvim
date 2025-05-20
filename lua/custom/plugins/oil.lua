28aa45c59eee395defac42612dd4c5ffe17e7a6d
BlackCorsair <BlackCorsair@users.noreply.github.com>
2025-05-20 17:20:41 +0200

    Add custom configuration

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    view_options = {
+      show_hidden = true,
+    },
+    vim.keymap.set('n', '<leader>o', '<cmd>Oil<CR>', { desc = '[S]earch Recent Files ("." for repeat)' }),
+  },
+}
